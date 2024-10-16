049ea7c16d75832c020784df79d3f42ae500c1cf
Jumponthemoon <qianchenghao123@gmail.com>
2024-10-16 23:36:12 +0100

    First commit

---
+++ scripts/gen_data_weather.sh
@@ -0,0 +1,6 @@
+
+
+CUDA_VISIBLE_DEVICES=0 python -m gen_data \
+    --work-dir /media/chenghao/Fanxiang/dataset \
+    --batch-size 1\
+    --sd-path /media/chenghao/Fanxiang/Projects/DATUM/checkpoints/cityscapes/inf_ckpt200 \
